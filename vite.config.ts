a2b8efd280d6c33fd93bba7ecf339b1a8cf969b5
Pedrinho Hilário <flygames1214@gmail.com>
2025-09-23 11:23:44 -0300

    ajustes para deploy no servidor de desenvolvimento

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/desenvolvimento/altm/',
   plugins: [
     react(),
     tailwindcss(),
