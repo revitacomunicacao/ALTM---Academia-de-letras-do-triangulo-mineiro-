c36d4985a3f80be7950446c750701cebb717a969
Pedrinho Hilário <flygames1214@gmail.com>
2025-09-24 11:48:42 -0300

    banners dinamicos vindo da área administrativa

--- src/app/home/components/Banner.tsx
+++ src/app/home/components/Banner.tsx
@@ -7,24 +7,28 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useContent } from "@/hooks/useContent"
 import { useEffect, useState } from "react"
+import { IBanners } from "../types/IBanners"
 
 export const Banner = () => {
 
-  const banners = [
-    {
-      img: banner1,
-      alt: "Banner 1 - Academia de Letras do Triângulo Mineiro"
-    },
-    {
-      img: banner2,
-      alt: "Banner 2 - Academia de Letras do Triângulo Mineiro"
-    },
-  ]
+  // const banners = [
+  //   {
+  //     img: banner1,
+  //     alt: "Banner 1 - Academia de Letras do Triângulo Mineiro"
+  //   },
+  //   {
+  //     img: banner2,
+  //     alt: "Banner 2 - Academia de Letras do Triângulo Mineiro"
+  //   },
+  // ]
 
   const [api, setApi] = useState<any>()
   const [current, setCurrent] = useState(0)
   const [count, setCount] = useState(0)
+  const { data: banners, loading, error, refetch } = useContent<IBanners>("banners")
 
   useEffect(() => {
     if (!api) {
@@ -39,6 +43,22 @@ export const Banner = () => {
     })
   }, [api])
 
+  if(loading) return (
+    <section className="w-full overflow-hidden -mt-8">
+      <div className="relative w-full">
+        <Skeleton className="w-full h-[400px] rounded-none" />
+        
+        {/* Skeleton para as setas de navegação */}
+        <div className="absolute left-2 sm:left-4 top-1/2 transform -translate-y-1/2">
+          <Skeleton className="w-10 h-10 sm:w-12 sm:h-12 rounded-full" />
+        </div>
+        <div className="absolute right-2 sm:right-4 top-1/2 transform -translate-y-1/2">
+          <Skeleton className="w-10 h-10 sm:w-12 sm:h-12 rounded-full" />
+        </div>
+      </div>
+    </section>
+  )
+
   return (
     <section className="w-full overflow-hidden -mt-8">
       <Carousel
@@ -51,12 +71,12 @@ export const Banner = () => {
         }}
       >
         <CarouselContent className="-ml-0">
-          {banners.map((banner, index) => (
-            <CarouselItem key={index} className="pl-0 basis-full">
+          {banners.map(({ banner_url,id,title }) => (
+            <CarouselItem key={id} className="pl-0 basis-full">
               <div className="border-0 shadow-none rounded-none w-full">
                 <img
-                  src={banner.img}
-                  alt={banner.alt}
+                  src={banner_url}
+                  alt={title}
                   className="w-full h-[400px] object-cover"
                 />
               </div>
---
+++ src/app/home/types/IBanners.ts
@@ -0,0 +1,6 @@
+export interface IBanners {
+  id: number;
+  title: string;
+  banner_url: string;
+  link: string;
+}
